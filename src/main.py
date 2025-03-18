c25357ba55c27a38bd8cfe82e18f70c2ff74b328
Yuliiiaaaa <as_ashes_00@mail.ru>
2025-03-18 11:07:30 +0300

    feat: dded new feature  and fixed bug

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Dict
 
 
 app = FastAPI()
@@ -13,28 +12,27 @@ class Operation(BaseModel):
 
 
 @app.post("/calculate")
-async def calculate(operation: Operation) -> Dict[str, str]:
+async def calculate(operation: Operation):
     operation_type = operation.operation_type
     x = operation.x
     y = operation.y
 
-    try:
-        if operation_type == "add":
-            result = x + y
-        elif operation_type == "subtract":
-            result = x - y
-        elif operation_type == "multiply":
-            result = x * y
-        elif operation_type == "divide":
-            if y == 0:
-                raise ValueError("Cannot divide by zero")
-            result = x / y
-        elif operation_type == "square_root":
-            if x < 0:
-                raise ValueError("square root of a negative number")
-            result = x * 0.5
-        else:
-            raise ValueError("Invalid operation type")
-        return {"result": str(result)}
-    except ValueError as e:
-        return {"error": str(e)}
+    result = None
+
+    if operation_type == "add":
+        result = x + y
+    elif operation_type == "subtract":
+        result = x - y
+    elif operation_type == "multiply":
+        result = x * y
+    elif operation_type == "divide":
+        if y == 0:
+            return {"error": "Cannot divide by zero"}
+        result = x / y
+    elif operation_type == "square_root":
+        if x < 0:
+            return {"error": "root of a negative number"}
+        result = x ** 0.5
+    else:
+        return {"error": "Invalid operation type"}
+    return {"result": result}
